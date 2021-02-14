4c7a36366c43c503f6526b454989490c55def172
Colten Appleby <colten96@gmail.com>
2021-02-14 19:55:41 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,2 +1,12 @@
 # Write your solution here
 
+import random
+
+chance_of_rain = random.random
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
+end
